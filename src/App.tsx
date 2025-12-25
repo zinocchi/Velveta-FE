ac4e762ff0167598d38bcd47c4a69793b9785f64
zinocchi <zinocchidev@gmail.com>
2025-12-25 22:28:05 +0700

    AKHIRNYA BISA LOGIN

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,23 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+// import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App;
 
+
+export default App;
 
 
 
